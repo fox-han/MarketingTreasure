0b8bf6efafa7de5d9b976a47d708eb0b4a66d03a
Han <sdtahwg@126.com>
2018-03-14 15:53:59 +0800

    Initial Commit

---
+++ MarketingTreasure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MarketingTreasure
+//
+//  Created by hanwenguang on 2018/3/14.
+//  Copyright © 2018年 hanwenguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MarketingTreasure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MarketingTreasure
+//
+//  Created by hanwenguang on 2018/3/14.
+//  Copyright © 2018年 hanwenguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
